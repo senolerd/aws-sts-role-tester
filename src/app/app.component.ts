888e1bea7b9d43c58e6f0e55ebc69bfc41b91b8d
Senol <whereimdb@gmail.com>
2023-10-27 00:20:50 -0500

    sts side added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { STSClient, STSClientConfig, AssumeRoleCommand, AssumeRoleCommandInput, AssumeRoleCommandOutput } from '@aws-sdk/client-sts'
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aws-sts-role-tester';
+
+  isStsButtonDisabled: Boolean = true
+  isStsSuccess:unknown = null
+
+  stsCredFG = new FormGroup({
+    access_key: new FormControl(),
+    secret_key: new FormControl(),
+    role_arn: new FormControl(),
+    region: new FormControl()
+  })
+
+  async form_check() {
+    let form_control = await Object.values(this.stsCredFG.value).filter(val => {
+      return val?.length == 0
+    })
+
+    this.isStsButtonDisabled = form_control.length == 0 ? false : true
+
+  }
+
+  stsAction() {
+    this.isStsSuccess = null
+    const sts_config:STSClientConfig = {
+      region : this.stsCredFG.value.region?.toString(),
+      credentials: {
+        accessKeyId: this.stsCredFG.value.access_key,
+        secretAccessKey: this.stsCredFG.value.secret_key
+      }
+    }
+    
+    const sts_client = new STSClient(sts_config)
+
+    const assume_role_command_input:AssumeRoleCommandInput = {
+      RoleArn: this.stsCredFG.value.role_arn,
+      RoleSessionName: "testingthewater"
+    }
+
+    sts_client.send( new AssumeRoleCommand(assume_role_command_input))
+      .then(response =>{
+        console.log(response)
+        this.isStsSuccess = true
+      })
+      .catch((Err:Error) =>{
+        console.log("Err.cause: ", Err.cause)
+        console.log("Err.message: ", Err.message)
+        console.log("Err.name: ", Err.name)
+        this.isStsSuccess = false
+      })
+
+
+  }
+
+  ngOnInit() {
+    this.stsCredFG.statusChanges.subscribe(_ => this.form_check())
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule} from "@angular/material/card"
+import { MatButtonModule } from "@angular/material/button"
+import { MatInputModule } from "@angular/material/input"
+import { MatSelectModule } from "@angular/material/select"
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from "@angular/material/icon"
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
